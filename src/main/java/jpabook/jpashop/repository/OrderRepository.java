7add293c16e9e4def30b2356927cec4f21686146
Jun981020 <flwnsgud@naver.com>
2023-04-03 22:11:13 +0900

    order v3 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -103,4 +103,15 @@ public class OrderRepository {
                         " join o.delivery d", OrderSimpleQueryDto.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithOrderItems() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i"
+                ,Order.class
+        ).getResultList();
+    }
 }
